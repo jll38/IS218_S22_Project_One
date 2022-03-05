598836279422c60c0af0be45fd2327a8aca2fd4c
Julian <jll38@njit.edu>
2022-03-04 19:53:09 -0500

    git, docker, index, and python pages updated

--- app.py
+++ app.py
@@ -5,17 +5,25 @@ app = Flask(__name__)
 
 bootstrap = Bootstrap5(app)
 
+#loads homepage
 @app.route("/")
 def index():
     return render_template('index.html')
+
+#loads Git page
 @app.route("/git")
 def git():
     return render_template('git.html')
+
+#loads docker page
 @app.route("/docker")
 def docker():
     return render_template('docker.html')
+
+#loads python page
 @app.route("/python")
 def python():
     return render_template('python.html')
+
 if __name__ == '__main__':
     app.run()
--- static/carousel.js
+++ static/carousel.js
@@ -1,7 +1,2 @@
-const buttons = document.querySelector("[data-slide]");
-
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        const offset = button.dataset.carouselButton === "next" ? 1 : -1
-    })
-})
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel)
