2dc2e09ccb45435a9af12a3248d10305aeb93391
Julian <jll38@njit.edu>
2022-03-07 13:44:09 -0500

    app

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask_bootstrap import Bootstrap5
 from flask import Flask, render_template
+from app.context_processors import utility_text_processors
+from app.simple_pages import simple_pages
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap5(app)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.register_blueprint(simple_pages)
+app.context_processor(utility_text_processors)
 
 #loads homepage
 @app.route("/")
