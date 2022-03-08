3fd47cc212386f2ee30c13ff60d027b194b7902f
Julian <jll38@njit.edu>
2022-03-07 19:34:53 -0500

    Heroku Fix Attempt #3

---
+++ tests/pytest.py
@@ -0,0 +1,47 @@
+"""This test the homepage"""
+
+def test_request_main_menu_links(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b'<a class="nav-link" href="/">Home</a>' in response.data
+    assert b'<a class="nav-link" href="/git">Git</a>' in response.data
+    assert b'<a class="nav-link" href="/docker">Docker</a>' in response.data
+    assert b'<a class="nav-link" href="/python">Python/Flask</a>' in response.data
+    assert b'<a class="nav-link" href="/cicd">CI/CD</a>' in response.data
+
+
+def test_request_git(client):
+    """This makes the index page"""
+    response = client.get("/git")
+    assert response.status_code == 200
+    assert b"About Git" in response.data
+
+def test_request_page1(client):
+    """This makes the index page"""
+    response = client.get("/docker")
+    assert response.status_code == 200
+    assert b"Docker" in response.data
+
+def test_request_page2(client):
+    """This makes the index page"""
+    response = client.get("/python")
+    assert response.status_code == 200
+    assert b"Python & Flask" in response.data
+
+def test_request_page3(client):
+    """This makes the index page"""
+    response = client.get("/cicd")
+    assert response.status_code == 200
+    assert b"Page 3" in response.data
+
+def test_request_page4(client):
+    """This makes the index page"""
+    response = client.get("/page4")
+    assert response.status_code == 200
+    assert b"CI/CD" in response.data
+
+def test_request_page_not_found(client):
+    """This makes the index page"""
+    response = client.get("/page5")
+    assert response.status_code == 404
